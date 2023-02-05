51d2a48f51559a4d5c302a9704cf2268a318142f
Nahum <nahumm01@outlook.com>
2023-02-05 18:56:34 -0300

    Começando a parte lógica do projeto

---
+++ script.js
@@ -0,0 +1,9 @@
+var textArea = document.querySelector(".textArea")
+var mensagem = document.querySelector(".mensagem")
+
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
